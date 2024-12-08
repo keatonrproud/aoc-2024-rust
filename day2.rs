4e11c6c9643ff18044931a256db3014b9087bfcf
kproBS <kpro@bendingspoons.com>
2024-12-08 15:04:46 +0100

    day2_2 works! (with help)

--- day2.rs
+++ day2.rs
@@ -27,32 +27,54 @@ fn read_data(file_path: &str) -> io::Result<Vec<Vec<i32>>> {
 
 /// Reports are safe if the Vector is entirely increasing or decreasing,
 /// and the difference for each step is between 1 and 3 (inclusive).
-fn assess_report_safety(report: Vec<i32>) -> io::Result<bool> {
-    let mut last_level: i32 = report[0];
-    let report_increases: bool = report[1] > last_level;
-
-    for level in report.iter().skip(1) {
-        let diff: i32 = level - last_level;
-
-        if diff.abs() > 3 || diff.abs() < 1 {
-            return Ok(false);
-        }
-
-        let currently_increasing: bool = diff > 0;
-
-        if currently_increasing != report_increases {
-            return Ok(false)
-        }
-
-        last_level = *level;
+/// Part 2: A report is safe if after removing one item it meets the above criteria.
+fn compare_level_safety(first_level: i32, next_level: i32, report_increases: bool) -> bool {
+    println!("First level {}, next level {}, report_increases {}", first_level, next_level, report_increases);
+    let diff: i32 = next_level - first_level;
+    if diff.abs() > 3 || diff.abs() < 1 {
+        println!("False");
+        return false;
     }
-    Ok(true)
+
+    let currently_increasing: bool = diff > 0;
+    if currently_increasing != report_increases {
+        println!("False");
+        return false;
+    }
+    println!("True");
+    true
+}
+
+fn assess_report_safety(report: Vec<i32>) -> bool {
+    let mut report_increases: bool = report[1] > report[0];
+
+    println!("Report: {:?}", report);
+    for (i, current_level) in report.iter().enumerate().skip(1) {
+        let last_level = report[i-1];
+        if let false = compare_level_safety(last_level, *current_level, report_increases) {
+            println!("Report is unsafe.");
+            return false;
+        }
+        report_increases = current_level > &last_level;
+    }
+    println!("Report is safe!");
+    true
+}
+
+fn assess_safety_with_dampening(report: Vec<i32>) -> bool {
+    /// Inspiration from a solution found online, much cleaner than my previous logic.
+    /// Checks if by removing any 1 of the items, if the report is safe.
+    (0..report.len()).any(|i| {
+        let mut tmp = report.clone();
+        tmp.remove(i);
+        assess_report_safety(tmp)
+    })
 }
 
 fn assess_reports(reports: Vec<Vec<i32>>) -> io::Result<i32> {
     let mut safe_count: i32 = 0;
     for report in reports {
-        if let Ok(true) = assess_report_safety(report) {
+        if let true = assess_safety_with_dampening(report) {
             safe_count += 1;
         }
     }
